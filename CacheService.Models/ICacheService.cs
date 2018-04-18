9a9a31b01bebdbed4d9e1ba26dd4110397bc20f7
Steve Finn <steve.d.finn@gmail.com>
2018-04-18 12:39:50 +1000

    initial Commit

---
+++ CacheService.Models/ICacheService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.FabricTransport;
+
+[assembly: FabricTransportServiceRemotingProvider(RemotingListener = RemotingListener.V2Listener, RemotingClient = RemotingClient.V2Client)]
+
+namespace CacheService.Models
+{
+
+    public interface ICacheService: IService
+    {
+        Task<string> Get(string key);
+
+        Task Store(string key, string value);
+        Task Delete(string key);
+    }
+}
---
+++ CacheService/CacheService.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Fabric;
+using System.Threading.Tasks;
+using CacheService.Models;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace CacheService
+{
+    /// <summary>
+    /// An instance of this class is created for each service replica by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class CacheService : StatefulService, ICacheService
+    {
+        public CacheService(StatefulServiceContext context)
+            : base(context)
+        { }
+
+       
+        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
+        {
+            return this.CreateServiceRemotingReplicaListeners();
+        }
+
+        public async Task<string> Get(string key)
+        {
+            var dictionary = await StateManager.GetOrAddAsync<IReliableDictionary<string, string>>("cache");
+            using (var tx = StateManager.CreateTransaction())
+            {
+                var result = await dictionary.TryGetValueAsync(tx, key);
+                await tx.CommitAsync();
+                return result.HasValue ? result.Value : string.Empty;
+            }
+        }
+
+        public async Task Store(string key, string value)
+        {
+            var dictionary = await StateManager.GetOrAddAsync<IReliableDictionary<string, string>>("cache");
+            using (var tx = StateManager.CreateTransaction())
+            {
+                await dictionary.AddOrUpdateAsync(tx, key, value,
+                    (key1, existing) => value);
+
+                await tx.CommitAsync();
+
+            }
+        }
+
+        public async Task Delete(string key)
+        {
+            var dictionary = await StateManager.GetOrAddAsync<IReliableDictionary<string, string>>("cache");
+            using (var tx = StateManager.CreateTransaction())
+            {
+                await dictionary.TryRemoveAsync(tx, key);
+
+                await tx.CommitAsync();
+
+            }
+        }
+    }
+}
